122af2c57f636206d80db64ccc902d8bf7e48ba0
Rivashjerrier <rivijerrier@gmail.com>
2023-08-04 17:50:56 +0000

    Parameterize and patch as decorators

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+test_client.py module
+"""
+
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    TestGithubOrgClient(unittest.TestCase) class and implementation
+    of the test_org method
+    """
+    @parameterized.expand([
+        ('google', {"login": "google"}),
+        ('abc', {"login": "abc"}),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org, mocked, result):
+        """
+        This method tests that GithubOrgClient.org returns the correct value
+        """
+        mocked.return_value = result
+        test_client = GithubOrgClient(org)
+        test = test_client.org()
+        self.assertEqual(test, result)
+        mocked.assert_called_once()
